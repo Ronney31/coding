3f5d1157029866be2011131ffa0674320bdceb65
Ronney31 <ranjankumar2411@gmail.com>
2018-05-06 21:19:42 +0530

    Create LessThan.py

---
+++ pattern/LessThan.py
@@ -0,0 +1,24 @@
+#printing LessThan sign
+0000X
+000X0
+00X00
+0X000
+X0000
+0X000
+00X00
+000X0
+0000X
+print "#"*15
+p=4
+l=-4
+for i in range(9):
+    for j in range(5):
+        if(p==j and i<5):
+            print ("X"),
+        elif(i>=5 and j==l):
+            print("@"),
+        else:
+            print ("0"),
+    print "\n"
+    p-=1
+    l+=1
