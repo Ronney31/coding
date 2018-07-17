019f66502de1fb78fec00447ac39132d583135eb
Ronney31 <ranjankumar2411@gmail.com>
2018-07-17 20:50:06 +0530

    Update subset.py

--- DP/subset.py
+++ DP/subset.py
@@ -4,7 +4,7 @@ Created on Mon Jul 16 12:51:09 2018
 
 @author: Sardar
 """
-
+#method 1
 def subsets(nums):
     res = []
     backtrack(res, nums, [], 0)
@@ -21,7 +21,7 @@ def backtrack(res, nums, stack, pos):
         # dont take nums[pos]
         backtrack(res, nums, stack, pos+1)
 
- 
+#method 2
 # simplified backtrack
 #def backtrack(res, nums, cur, pos):
     # if pos >= len(nums):
@@ -30,7 +30,7 @@ def backtrack(res, nums, stack, pos):
         # backtrack(res, nums, cur+[nums[pos]], pos+1)
         # backtrack(res, nums, cur, pos+1)
  
- 
+#method 3
 # Iteratively
 def subsets2(self, nums):
     res = [[]]
